e690de9d4f56a63ba616d5fb9a20bad04787da49
JorgeOteiza <oteiza.jor@gmail.com>
2023-11-03 01:07:38 -0300

    responsive y celdas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Home() {
 	return (
-		<div className="container">
-			<div className="row d-flex justify-content-center align-items-center h-100vh">
+		<div className="container-fluid min-vh-100 d-flex justify-content-center align-items-center">
+			<h1 className="text-center mb-1">Tic Tac Toe</h1>
+			<div className="row row justify-content-center align-items-center">
 				<div className="col sm-4 mx-auto">
-					<h1 className="title">Tic Tac Toe</h1>
 					<div id="board">
 						{[...Array(9)].map((_, index) => (
-							<div className="cells" key={index}></div>
+							<div className="col-4 cells border border-dark" key={index}></div>
 						))}
 					</div>
 
