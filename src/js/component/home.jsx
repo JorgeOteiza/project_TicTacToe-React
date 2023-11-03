446ec03c696c834fa1150ba22fce056f890b5a6a
JorgeOteiza <oteiza.jor@gmail.com>
2023-11-03 00:15:24 -0300

    avance contenedor y celdas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+function Home() {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row d-flex justify-content-center align-items-center h-100vh">
+				<div className="col sm-4 mx-auto">
+					<h1 className="title">Tic Tac Toe</h1>
+					<div id="board">
+						{[...Array(9)].map((_, index) => (
+							<div className="cells" key={index}></div>
+						))}
+					</div>
+
+				</div>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
